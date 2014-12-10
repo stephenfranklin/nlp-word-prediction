4d0d2055c6db9a37a18c7d45e644278cafa130c3
Stephen Franklin <plamtruepub@gmail.com>
2014-12-10 14:41:23 -0600

    action button triggers a paste function which takes the text input (reactively) + a variable of char and outputs the combined text.

--- word-predict/server.R
+++ word-predict/server.R
@@ -1,7 +1,27 @@
 library(shiny)
 
+paste_it <- function(intext, word){
+    paste(intext,word,sep = " ")
+}
+
 shinyServer(
     function(input, output) {
-        output$text1 <- renderText({input$text1})
+        word1 <- "fuck"
+        word2 <- "shit"
+        
+        intext <- reactive({input$text1})
+        output$text1 <- renderText(intext()) ## no comma here
+        output$text2 <- renderText({
+            if (input$word1but == 0) "You haven't pressed the button." 
+            else if (input$word1but == 1) word1 
+            else if (input$word1but == 2) word2             
+            else "OK quit pressing it."
+        }) 
+        output$text3 <- renderText({
+            if (input$word1but == 0) "You haven't pressed the button." 
+            else if (input$word1but == 1) paste_it(intext(),word1)
+            else if (input$word1but == 2) paste_it(intext(),word2)          
+            else "OK quit pressing it."
+        }) 
     }
 )
--- word-predict/ui.R
+++ word-predict/ui.R
@@ -1,16 +1,18 @@
 shinyUI(pageWithSidebar(
     headerPanel("Word Prediction"),
     sidebarPanel(
-        textInput(inputId='text1', label='Input Text1'), ## watch the commas!
-        actionButton('goButton', 'word 1'),
-        actionButton('goButton', 'word 2'),
-        actionButton('goButton', 'word 3'),
-        actionButton('goButton', 'word 4'), 
-        actionButton('goButton', 'word 5'),
-        actionButton('goButton', 'word 6')
+        textInput(inputId="text1", label = "Enter text here"),
+        tags$head(tags$style(type="text/css", "#text1 {width: 90%}")),
+        actionButton('word1but', 'Predict')
         ),
     mainPanel(
         p('Output text1'),
-        textOutput('text1') ## watch the commas!       
+        textOutput('text1'),
+        br(),
+        p('Output text2'),
+        textOutput('text2'), ## watch the commas!
+        br(),
+        p('Output text3'),
+        textOutput('text3') ## watch the commas! 
     )
 ))

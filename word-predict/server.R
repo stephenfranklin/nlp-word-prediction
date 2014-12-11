595eaac89245165cecefbc520e2e5381680c18b9
Stephen Franklin <plamtruepub@gmail.com>
2014-12-10 18:35:54 -0600

    Basic working prediction acheived. Needs: add 2gram prediction; display top 6 results. Wants: combo input/output text field; buttons display predicts;        bottom 20% non-unique unigrams associated by snippet.

--- word-predict/server.R
+++ word-predict/server.R
@@ -1,8 +1,8 @@
 library(shiny)
 
-source("paste_it.R")
 source("word_prediction.R")
-### This function predicts the next word from a trigram input.
+### Function `predict_w4(input_text, ngram4.dt)`
+### predicts the next word from a trigram input.
 ### arguments:  1) input_text, char;
 ###             2) ngram4.dt,, data.table of 4grams and their frequencies.
 ### 1) processes the input text,
@@ -14,28 +14,15 @@ load("tot.freqs70fmin05w4cull.RData")
 ### That loads the data.table of 4grams and frequencies
 ### `tot.freqs_70_fmin05_w4_culled`
 
-#paste_it <- function(intext, word){
-#    paste(intext,word,sep = " ")
-#}
-
 shinyServer(
     function(input, output) {
-        word1 <- "fuck"
-        word2 <- "shit"
-        stuff <- reactive(tot.freqs_70_fmin05_w4_culled$ngrams[1])
+        stuff <- reactive(tot.freqs_70_fmin05_w4_culled$ngrams[200])
         intext <- reactive({input$text1})
         output$text1 <- renderText(intext()) ## no comma here
         output$text2 <- renderText({
-            if (input$word1but == 0) "You haven't pressed the button." 
-            else if (input$word1but == 1) word1 
-            else if (input$word1but == 2) word2             
-            else "OK quit pressing it."
-        }) 
-        output$text3 <- renderText({
-            if (input$word1but == 0) "You haven't pressed the button." 
-            else if (input$word1but %%2 == 1 ) paste_it(intext(),word1)
-            else paste_it(intext(),word2)
-        }) 
+                word <- predict_w4(intext(),tot.freqs_70_fmin05_w4_culled)
+                word[1]
+        })
         output$text4 <- renderText(stuff()) ## no comma here
     }
 )

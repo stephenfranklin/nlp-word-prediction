94457b2d65301e50ca8df3a36a7e842b60f5addc
Stephen Franklin <plamtruepub@gmail.com>
2014-12-13 19:45:22 -0600

    Back off working. server.R updated with the 100 data. Empty text field results in this annoyance: object 'input.dt' not found

--- word-predict/server.R
+++ word-predict/server.R
@@ -10,14 +10,14 @@ source("word_prediction.R")
 ### 3) selects from an existing data.table of 4grams
 ###     those that match the input trigram, and
 ### 4) returns a list `tops` of the 4th words ordered by most frequent.
-load("tot.freqs70fmin05w4cull.RData")
+load("tot.freqs_100_10w.RData")
 ### That loads the data.table of 4grams and frequencies
-### `tot.freqs_70_fmin05_w4_culled`
+### `tot.freqs`
 
 random_4gram <- function(){
-    n_tot <- nrow(tot.freqs_70_fmin05_w4_culled)
+    n_tot <- nrow(tot.freqs)
     randnum<-round(runif(1,1,n_tot),0)
-    r4gram <- tot.freqs_70_fmin05_w4_culled$ngrams[randnum]
+    r4gram <- tot.freqs$ngrams[randnum]
     r4gram
 }
 
@@ -25,27 +25,27 @@ shinyServer(
     function(input, output) {
         intext <- reactive({input$text1})
         output$text1 <- renderText({
-            word <- predict_w4(intext(),tot.freqs_70_fmin05_w4_culled)
+            word <- predict_w4(intext(),tot.freqs)
             word[1]
         })
         output$text2 <- renderText({
-                word <- predict_w4(intext(),tot.freqs_70_fmin05_w4_culled)
+                word <- predict_w4(intext(),tot.freqs)
                 word[2]
         })
         output$text3 <- renderText({
-            word <- predict_w4(intext(),tot.freqs_70_fmin05_w4_culled)
+            word <- predict_w4(intext(),tot.freqs)
             word[3]
         })
         output$text4 <- renderText({
-            word <- predict_w4(intext(),tot.freqs_70_fmin05_w4_culled)
+            word <- predict_w4(intext(),tot.freqs)
             word[4]
         })
         output$text5 <- renderText({
-            word <- predict_w4(intext(),tot.freqs_70_fmin05_w4_culled)
+            word <- predict_w4(intext(),tot.freqs)
             word[5]
         })
         output$text6 <- renderText({
-            word <- predict_w4(intext(),tot.freqs_70_fmin05_w4_culled)
+            word <- predict_w4(intext(),tot.freqs)
             word[6]
         })
         output$text7 <- renderText({

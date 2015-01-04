40430ba36f485a5808798db8fed64a40857098e0
Stephen Franklin <plamtruepub@gmail.com>
2015-01-04 12:32:49 -0600

    more layout work

--- word-predict/ui.R
+++ word-predict/ui.R
@@ -1,10 +1,10 @@
-shinyUI(pageWithSidebar(
-    headerPanel("Word Prediction"),
+shinyUI(fluidPage(
+    titlePanel("Word Prediction"),
     mainPanel(
         width=6
         ,uiOutput("topPanel")
         ,p()
-        ,tags$textarea(id="text1", rows=4, cols=80, "Give it a ")
+        ,tags$textarea(id="text1", rows=6, cols=80, "Give it a ")
         ,p()
         ,uiOutput("bottomPanel")
         ,p()
@@ -12,13 +12,18 @@ shinyUI(pageWithSidebar(
                     choices = c(TRUE, FALSE))
         ,numericInput(inputId="num_bab", label="Number of words to babble:",
                       value=4,min=1,max=100)
+        
     ),
     sidebarPanel(
+        tabsetPanel(id = "explanation", position="above"
+            ,tabPanel("Prediction", includeText("prediction"))
+            ,tabPanel("Processing the Data", includeText("processing"))
         #    width=3
         #textInput(inputId="text1", label = "Enter text here"),
         #tags$head(tags$style(type="text/css", "#text1 {width: 90%}")),
         #,actionButton('random.btn', 'Random 4gram')
         #,p()
         #,textOutput('text7')
+        )
     )
 ))

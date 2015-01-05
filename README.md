55ef466f9d81074e5ec091c44c58094f0762dff0
Stephen Franklin <plamtruepub@gmail.com>
2015-01-04 20:05:31 -0600

    fixed reactivity problem caused by `includeHTML()`.

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ nlp-word-prediction
 Coursera / Johns Hopkins Data Science capstone project
 
 This is a shiny app that predicts probable next words from two or three words
-that are typed by the user. The app uses a hard-wona set 4grams with
-frequencies processed from a big set of excerpts from Twitter, blogs, and news.
+that are typed by the user.
+The app uses a set of 4grams and frequencies processed from a big set of
+ excerpts from Twitter, blogs, and news.
--- word-predict/ui.R
+++ word-predict/ui.R
@@ -17,8 +17,8 @@ shinyUI(fluidPage(
     sidebarPanel(
         width=6
         ,tabsetPanel(id = "explanation", position="above"
-            ,tabPanel("Prediction", includeHTML("readme_prediction.html"))
-            ,tabPanel("Processing the Data", includeHTML("readme_processing.html"))
+            ,tabPanel("Prediction", includeMarkdown("readme_prediction.Rmd"))
+            ,tabPanel("Processing the Data", includeMarkdown("readme_processing.Rmd"))
         )
     )
 ))
